feb8c44eee6350027fd99d89c8b02b72a6683662
BuildTools <theonlygusti@gmail.com>
2017-01-14 18:04:59 +0000

    First plugin.py. Basic skeleton file 💀

---
+++ plugin.py
@@ -0,0 +1,10 @@
+def results(parameters, original_query):
+    return {
+        "webivew_transparent_background": True,
+        "title": "xkcd",
+        "run_args": [],
+        "html": "None"
+        }
+
+def run():
+    pass
